9dbc3f3eb61a9ea4ce23bbf7b95a8ff68f0e835e
xi72yow <65042627+xi72yow@users.noreply.github.com>
2021-05-18 13:57:21 +0200

    Create kickstart.sh

---
+++ kickstart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#chmod +x make script
+sudo apt update
+sudo apt upgrade
+
+#dependencies puppeteer
+sudo apt install ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils git nodejs npm-y
+
+#get the GG
+git clone https://github.com/xi72yow/GamesGatherer
+cd GamesGatherer
+npm install forever -g
+npm install
+
+#set ip-address
+nano public/index.html
+#start server
+forever start server.js 
+
